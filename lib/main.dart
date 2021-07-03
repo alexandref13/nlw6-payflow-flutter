a9cb4ef73b588cf8925df25f33a6ebcbb6710189
alexandref13 <alexandrefilhoalvo@gmail.com>
2021-07-03 02:22:42 -0300

    Aula 02

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:payflow/home/home.page.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
 
 void main() {
   runApp(GetMaterialApp(
+    debugShowCheckedModeBanner: false,
     initialRoute: '/',
     getPages: [
-      GetPage(name: '/', page: () => Home()),
+      GetPage(name: '/', page: () => SplashPage()),
+      GetPage(name: '/login', page: () => LoginPage()),
+      GetPage(name: '/home', page: () => HomePage()),
     ],
   ));
 }
